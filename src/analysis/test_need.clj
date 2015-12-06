fd7e7fa6efb9ed69bc49c43235f3eeb3f47daa79
Michael Bohn <michael.bohn@freiheit.com>
2015-12-06 20:07:39 +0100

    Detailed test-need output

--- src/analysis/test_need.clj
+++ src/analysis/test_need.clj
@@ -24,10 +24,10 @@
            :bugfixes (count (filter bugfix-commit? log)))))
 
 (defn- join-bugfix-commit-data
-  "Joins commit info to coverage data, takes only the first 750 coverage data since the commit data retrieval
+  "Joins commit info to coverage data, takes only the first 1000 coverage data since the commit data retrieval
   is very expensive"
   ([git-repo coverage-data & [commit-count]]
-   (let [n (or commit-count 750)]
+   (let [n (or commit-count 1000)]
      (->> coverage-data
           (map (partial add-commit-data git-repo))
           (take n)))))
@@ -46,13 +46,24 @@
   (doseq [line lines]
     (println line)))
 
+(defn- test-need-result [result]
+  (str (:class result) " has" (:uncovered result) " uncovered lines,
+        \tit took " (:commits result) " commits and " (:bugfixes result) " bugfixes to produce this class
+        \tgiving it a bugfix/uncovered line ratio of: " (double (bugfix-per-uncovered-line result)) "\n"))
+
+(defn- suspense-dots [limit all-count]
+  (str "... only " limit "/" all-count " shown"))
+
 (defn print-analyse-test-need [coverage-file git-repo & [result-count]]
   "Same as function without print, but formats result in a readable way"
-  (let [n (or result-count 25)
+  (let [n (or result-count 20)
         result (analyse-test-need coverage-file git-repo)
         by-bugfix (reverse (sort-by bugfix-per-uncovered-line result))]
     (print-lines
      [["Ranked purely by most uncovered lines, you should write tests for these classes: "
-       (take n result)
+       (map test-need-result (take n result))
+       (suspense-dots n (count result))
+       "\n"
        "Considering bugfixes for uncovered lines, you should write tests for these classes: "
-       (take n by-bugfix)]])))
+       (map test-need-result (take n by-bugfix))
+       (suspense-dots n (count by-bugfix))]])))
